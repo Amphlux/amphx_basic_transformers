6e2335d736ed00379f0f152fe2172cebb837f9d6
Amphlux <115769719+Amphlux@users.noreply.github.com>
2022-10-14 22:27:42 -0500

    Create Zero_Shot_Classification_BART_large_MNLI.py

---
+++ Zero_Shot_Classification_BART_large_MNLI.py
@@ -0,0 +1,23 @@
+#load the zero-shot-classification pipeline
+from transformers import pipeline
+classifier = pipeline("zero-shot-classification",
+                      model="facebook/bart-large-mnli")
+
+#create array named label_array
+label_array = []
+#request number of labels from user
+array_n = int(input("Enter number of labels to use: \n"))
+#request each label from user and put them into label_array
+for i in range(0, array_n):
+    labelname = input("Input label for use: \n")
+    label_array.append(labelname)
+
+#request a sequence to be zero shot classified
+sequence_to_classify = input("Input a sequence/sentence to zero shot classify: \n")
+#If more than one candidate label can be correct, pass multi_class=True to calculate each class independently
+if str.upper(input("Do you want to use multi labeling? y/n \n")) == "Y":
+    mlabelbool = True
+else:
+    mlabelbool = False
+
+classifier(sequence_to_classify, label_array, multi_label=mlabelbool)
